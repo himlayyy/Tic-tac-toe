b097ed861cb5f7eaa7e3f63876814dee4cd64e41
himlayyy <alesandralelis@gmail.com>
2022-03-07 18:45:39 +0800

    Added more styling

--- script.js
+++ script.js
@@ -1,23 +1,69 @@
-class Player{
-    constructor(){
+class Player {
+    constructor() {
         this.inputs = [];
     }
-    moves(id){
+    moves(id) {
         const move = parseInt(id)
-        if(!player.inputs.includes(move)) this.inputs.push(move);        
-        console.log(player.inputs);
+        if (!this.inputs.includes(move)) this.inputs.push(move);
+        console.log(this.inputs);
     }
 }
-class Game{
-    constructor(){
+
+class Game {
+    constructor() {
         this.inputs = [];
         this.won = false;
         this.over = false;
         this.moves = 0;
+        this.player = new Player();
         Game.drawBoard();
-        
     }
-    static drawBoard(){
+    handleClick(id, cell) {
+        console.log(cell);
+        cell.classList.add("player");
+        let value = Cell.getValue(id);
+        if (value == "") Cell.addInput(id, "x");
+        this.player.moves(id);
+        console.log(this.player.inputs.length);
+        if (this.player.inputs.length >= 3) {
+            console.log('here');
+            Game.checkWin(this.player.inputs);
+        }
+        this.gameState();
+        this.moves += 1;
+    }
+    gameState() {
+        let message;
+        if (this.won == true) {
+            this.over = true;
+            message = "You won!";
+            Game.clearBoard(message);
+        };
+    }
+    restart() {
+        this.inputs.length = 0;
+        this.won = false;
+        this.over = false;
+        this.moves = 0;
+        this.player.inputs = 0;
+
+    }
+    // static playerMoves() {
+    //     const cells = document.querySelectorAll(".cell");
+    //     cells.forEach(cell => {
+    //         cell.addEventListener("click", function (cell) {
+    //             let id = cell.target.dataset.index;
+    //             game.handleClick(id, cell.target);
+    //         })
+    //     })
+
+    // }
+    static clearBoard(message) {
+        let result = document.querySelector("#result");
+        result.innerHTML = message;
+        setTimeout(() => Game.newGame(), 3000);
+    }
+    static drawBoard() {
         console.log("Drew board");
         let board = document.querySelector("#board");
         board.innerHTML = `<div class="cell" data-index="1"></div>
@@ -30,74 +76,66 @@ class Game{
         <div class="cell" data-index="8"></div>
         <div class="cell" data-index="9"></div>`
     }
-    
-    static newGame(){
-        let board = document.querySelector("#board");
-        while (board.firstChild) {
-            board.firstChild.remove()
-        }
-        Game.drawBoard();          
+    static newGame() {
+        // let board = document.querySelector("#board");
+        // while (board.firstChild) {
+        //     // board.firstChild.remove()
+        //     console.log(board.firstChild.innerHTML);
+        // }
+        cells.forEach(cell => {
+            console.log(cell.innerHTML);
+            cell.innerHTML = "";
+        })
+        Game.drawBoard();
     }
 
-    static handleClick(id, cell){        
-        console.log(cell);
-        cell.classList.add("player");
-        let value = Cell.getValue(id);
-        if(value == "") Cell.addInput(id, "x");
-        player.moves(id);
-        console.log(player.inputs.length);
-        if (player.inputs.length >= 3){   
-            console.log('here');     
-            Game.checkWin(player.inputs);
-        }
-        Game.gameState();
-        game.moves += 1;
-    }
-    static gameState(){
-        if(game.won == true) {
-            game.over = true;
-            console.log("Game over");
-            Game.newGame();
-        };
-    }
-    static checkWin(playerInputs){
+    static checkWin(playerInputs) {
         console.log("in check win");
         console.log(playerInputs);
         const combos =
-       [ [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7]];        
+            [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]];
         combos.forEach(combo => {
             // const result = combo.includes(val => combo.every(val));
             if (combo.every(val => playerInputs.includes(val))) game.won = true;
         });
-                
     }
 }
-class Cell{
-    static addInput(id, input){
+class Cell {
+    static addInput(id, input) {
         let cell = document.querySelector(`div[data-index="${id}"]`);
         cell.innerHTML = input;
-        game.inputs[id-1] = input;
+        game.inputs[id - 1] = input;
         console.log(game.inputs);
     }
-    static getValue(id){
-        let cell = document.querySelector(`div[data-index="${id}"]`); 
+    static getValue(id) {
+        let cell = document.querySelector(`div[data-index="${id}"]`);
         return cell.innerHTML;
     }
 }
 
 let game = new Game();
-let player = new Player();
+// let gam = new Game();
+// Game.playerMoves();
+
+document.querySelector("#restart").addEventListener("click", () => {
+    console.log(game);
+    game.restart();
+    console.log(game);
+    Game.newGame();
+    // Game.drawBoard();
+
+});
 
 const cells = document.querySelectorAll(".cell");
 cells.forEach(cell => {cell.addEventListener("click", function(cell){
-    console.log(player.inputs);
     let id = cell.target.dataset.index;
-    Game.handleClick(id, cell.target);
+    game.handleClick(id, cell.target);
 })})
+
--- tictac.js
+++ tictac.js
@@ -1,6 +1,7 @@
 class Player {
-    constructor() {
+    constructor(letter) {
         this.moves = [];
+        this.letter = letter;
     }
 }
 class Game {
@@ -8,43 +9,84 @@ class Game {
         this.usedCells = [];
         this.over = false;
         this.totalMoves = 0;
-        this.player = new Player();
-        this.other = new Player();
-        this.players = [this.player, this.other];
+        this.gameWon = false;
+        // this.player = new Player("y");
+        // this.other = new Player();
+        // this.players = [this.player, this.other];
+        this.player1 = new Player("x");
+        this.player2 = new Player("o");
+        this.active = this.player1;
         Board.drawBoard();
+    }
+
+
+    // To do before ko makalimot ani ugma. So what im trying to do with setActive is make player point to a different instance of Player()
+
+    // Ang point nga dili nako usbon ang player sa class functions is kapoy hahaha
+
+    // I'm trying to see if player.property works. ang intended result is like this: player.letter can switch between this.player1.letter OR this.player2.letter
+    setActive(){
+        let active;
+        if (this.active == this.player1){
+            active = this.player2;
+        }
+        else{
+            active = this.player1;
+        }
+        this.active = active;
     }
     handleClick(id) {
         if (this.over == true) {
             Board.disableBoard();
         }        
         else{
-            if (Board.getCellVal(id) === "") Board.addCellVal(id, "X");
+            // if (Board.getCellVal(id) === "") Board.addCellVal(id, this.player.letter);
+            if (Board.getCellVal(id) === "") Board.addCellVal(id, this.active.letter);
             this.newMove(parseInt(id));
-
-            if (this.totalMoves >= 3) {
-                this.checkWin(this.player.moves);
+            // So nauna ug switch before siya midung sa 
+            console.log("New Move added");
+            console.log(this.totalMoves)
+            
+            if (this.totalMoves === 9 ){
+                console.log("total moves = 9")
+                this.won = false;
+                this.gameResult(this.won, "");
+                console.log(this.won);
             }
+
+            else if (this.totalMoves >= 3) {
+                // this.checkWin(this.player.moves);
+                console.log("total moves are 3+")
+                this.checkWin(this.active.moves, this.active.letter);
+            }
+            this.setActive();
         }
     }
     newMove(id) {
         console.log(this.usedCells.includes(id))
 
         if (!this.usedCells.includes(id)) {
-            this.player.moves.push(id);
+            // this.player.moves.push(id);
+            this.active.moves.push(id);
             this.usedCells.push(id);
             this.totalMoves += 1;
+            
             console.log('new move added');
+            console.log(this.active);
+            console.log(this.active.moves);
         }
     }
     restartGame() {
+        this.active = this.player1;
         this.usedCells.length = 0;
         this.over = false;
         this.totalMoves = 0;
-        this.player.moves.length = 0;
+        this.player1.moves.length = 0;
+        this.player2.moves.length = 0;
 
         Board.redrawBoard();
     }
-    checkWin(playerMoves) {
+    checkWin(playerMoves, letter) {
         console.log("in checkWin");
         const combos =
             [[1, 2, 3],
@@ -55,16 +97,32 @@ class Game {
             [3, 6, 9],
             [1, 5, 9],
             [3, 5, 7]];
-        console.log(playerMoves);
+        console.log(playerMoves, letter);
         combos.forEach(combo => {
             if (combo.every(val => playerMoves.includes(val))) {
                 this.over = true;
+                this.won = true;
+                console.log(playerMoves);
                 console.log(combo);
-                console.log("You won");
-                Board.disableBoard();
+                this.gameResult(this.won,letter)
             };
         })
-
+    }
+    gameResult(result, letter){
+        console.log("in game result!")
+        console.log(result);
+        let res = document.querySelector("#result");
+        let message = document.createElement("div");
+        if(result == true){
+            message.innerText = `${letter} has won!`;
+        }
+        else{
+            message.innerText = "It's a tie";
+        }
+        res.append(message);
+        console.log(message);
+        
+        Board.disableBoard();
     }
 }
 
@@ -109,6 +167,11 @@ class Board {
             cell.classList.remove("player");
             cell.textContent = "";
         })
+
+        let res = document.querySelector("#result");
+        console.log(res.child);
+        if(res.hasChildNodes()) res.firstChild.remove();
+       
         Board.drawBoard();
     }
     static disableBoard() {
